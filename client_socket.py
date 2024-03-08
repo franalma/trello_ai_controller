eb3b20d67185d9fa2e3637c07d40b8581e406fde
FranAlma <franalmeida@gmail.com>
2024-03-08 13:25:39 +0100

    changes in client

--- client_socket.py
+++ client_socket.py
@@ -6,20 +6,23 @@ import time
 class Client:
     
     # HOST = "101.46.143.21"
-    HOST = "192.168.2.126"  
+    # HOST = "192.168.2.126"  
+    HOST = "192.168.2.31"  
     # LOCALHOST = "127.0.0.1"
     # HOST = "127.0.0.1"  
     PORT_SEND = 65432  
     PORT_RECEIVED = 65431  
     s_out = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     s_out.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1000000)
+    
+    
     s_in = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)          
 
         
     def send(self, data): 
-        print(f"sending...{len(data)}")   
+        # print(f"sending...{len(data)}")   
         self.s_out.sendall(data)
-        time.sleep(1/4)
+        # time.sleep(1)
       
         
 
@@ -32,12 +35,18 @@ class Client:
             if data:
                 print(data)
                 time.sleep(2)
+                
+    def receive2(self):                
+        while True:
+            data, _ = self.s_out.recvfrom(1024)
+            print(data.decode("utf-8"))
+            
         
     
     def start(self):        
         self.s_out.connect((self.HOST, self.PORT_SEND))            
-        threadRecv = threading.Thread(target=self.receive)
-        # threadRecv.start()
+        threadRecv = threading.Thread(target=self.receive2)
+        threadRecv.start()
    
             
         
--- server_socket.py
+++ server_socket.py
@@ -2,7 +2,7 @@ import socket
 import time
 
 class Server:
-    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+    HOST = "192.168.2.91"  # Standard loopback interface address (localhost)
     PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
     PORT_CLIENT = 65431
     cont = 0 
